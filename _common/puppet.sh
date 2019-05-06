7a3d0fe92828ac7d15325d950cbeb288af7a1d6a
Fabian van der Hoeven <fabian.vanderhoeven@vermont24-7.com>
2019-05-06 14:41:46 +0200

    Added puppet-agent installation and timestamp to build image

---
+++ _common/puppet.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -eux
+
+major_version="`sed 's/^.\+ release \([.0-9]\+\).*/\1/' /etc/redhat-release | awk -F. '{print $1}'`";
+
+#Install puppet repo
+case "$major_version" in
+    6)
+    yum -y localinstall http://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
+    ;;
+    7)
+    yum -y localinstall http://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
+    ;;
+esac
+
+#Install puppet-agent
+yum install -y puppet-agent
